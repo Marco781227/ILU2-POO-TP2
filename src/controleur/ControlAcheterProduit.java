f9790e3469185fa2f5fa313f0d252b7ac2b2c261
REGRAGUI MARTINS Marco <marco781227@gmail.com>
2023-10-27 14:53:20 +0200

    TP3 terminé TEST commencé

--- src/controleur/ControlAcheterProduit.java
+++ src/controleur/ControlAcheterProduit.java
@@ -1,6 +1,8 @@
 package controleur;
 
+import villagegaulois.Etal;
 import villagegaulois.Village;
+import personnages.Gaulois;
 
 public class ControlAcheterProduit {
 	private Village village;
@@ -17,6 +19,20 @@ public class ControlAcheterProduit {
 
 	public boolean verifierIdentite(String nomAcheteur) {
 		return controlVerifierIdentite.verifierIdentite(nomAcheteur);
+	}
+	
+	public String[] trouverVendeursProduit(String produit) {
+		Gaulois[] vendeurs=village.rechercherVendeursProduit(produit);
+		String[] nomVendeurs= new String[vendeurs.length];
+		for (int i=0;i<vendeurs.length;i++) {
+			nomVendeurs[i]=vendeurs[i].getNom();
+		}
+		return nomVendeurs;
+	}
+	public int acheterProduit(String nomVendeur,int quantiteAcheter) {
+		Etal etal = controlTrouverEtalVendeur.trouverEtalVendeur(nomVendeur);
+		int quantiteVendu = etal.acheterProduit(quantiteAcheter);
+		return quantiteVendu;
 	}
 	
 }	
--- src/frontiere/BoundaryAcheterProduit.java
+++ src/frontiere/BoundaryAcheterProduit.java
@@ -14,5 +14,39 @@ public class BoundaryAcheterProduit {
 
 	public void acheterProduit(String nomAcheteur) {
 		System.out.println("Quel produit souhaitez-vous acheter ?");
+		String produit = scan.nextLine();
+		String[] nomVendeurs=controlAcheterProduit.trouverVendeursProduit(produit);
+		if (nomVendeurs.length==0) {
+			System.out.println("Désolé, personne ne vend ce produit au marché.\n");
+			return;
+		}
+		else {
+			StringBuilder dialogChoix = new StringBuilder("Chez quel commerçant voulez-vous acheter des fleurs?\n");
+			for (int i=0;i<nomVendeurs.length;i++) {
+				dialogChoix.append(i+1 + " - " + nomVendeurs[i] + "\n");
+			}
+			int choix = Clavier.entrerEntier(dialogChoix.toString());
+			choix--;
+			while(choix<0 && choix>=nomVendeurs.length){
+				System.out.println("Veuillez entrer un chiffre valide.\n");
+				choix = Clavier.entrerEntier(dialogChoix.toString());
+				choix--;
+			}
+			String vendeur = nomVendeurs[choix];
+			StringBuilder dialogAchat = new StringBuilder(nomAcheteur + " se déplace jusqu'à l'étal du vendeur "+vendeur+"\n");
+			dialogAchat.append("Combien de "+ produit+ " voulez-vous acheter?\n");
+			int quantiteAcheter = Clavier.entrerEntier(dialogAchat.toString());
+			int quantiteVendu = controlAcheterProduit.acheterProduit(vendeur, quantiteAcheter);
+			if (quantiteVendu==0) {
+				System.out.println(nomAcheteur+" veut acheter "+quantiteAcheter+" "+produit+", malheureusement il n'y en a plus!\n");
+			}
+			else if (quantiteVendu<quantiteAcheter) {
+				System.out.println(nomAcheteur+" veut acheter 5 fleurs, malheureusement "+vendeur+" n'en a plus que "+quantiteVendu
+						+". "+nomAcheteur+" achète tout le stock de Bonemine.\n");
+			}
+			else {
+				System.out.println(nomAcheteur+" achète "+quantiteVendu+" "+produit+ " à "+vendeur+".\n");
+			}
+		}
 	}
 }
